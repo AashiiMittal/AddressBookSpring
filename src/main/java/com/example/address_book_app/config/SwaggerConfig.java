349a16914705453a515bbb0a1befe059e7c202f9
AashiiMittal <113659511+AashiiMittal@users.noreply.github.com>
2025-03-10 11:08:52 +0530

    Update SwaggerConfig.java

--- src/main/java/com/example/address_book_app/config/SwaggerConfig.java
+++ src/main/java/com/example/address_book_app/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("EmployeePayRollAPI")
+                        .title("AddressBookAPI")
                         .version("1.0")
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
