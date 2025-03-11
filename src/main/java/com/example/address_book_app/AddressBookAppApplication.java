05c7a43f4a6764c77962555a5f9c3de792c2c2c0
AashiiMittal <aashimittal2004@gmail.com>
2025-03-11 08:42:50 +0530

    added logger patterns and different configuration for prod, dev and staging environments

--- src/main/java/com/example/address_book_app/AddressBookAppApplication.java
+++ src/main/java/com/example/address_book_app/AddressBookAppApplication.java
@@ -2,12 +2,16 @@ package com.example.address_book_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address-Book App Started in {} Environment",
+				context.getEnvironment().getProperty("spring.profiles.active"));
 	}
-
 }
